1b7f704ed984ed362f68a7def5e89e7e63df92d6
NigelVH <nigelvh@users.noreply.github.com>
2014-01-30 10:45:59 -0800

    Fixed tab vs space formatting error.

--- amprupdate/amprupdate.py
+++ amprupdate/amprupdate.py
@@ -158,14 +158,14 @@ def main():
         for dstaddress, interface in routes_to_add:
             commands.append("/interface ipip add local-address=%s name=ampr-%s remote-address=%s" % (edge_router_ip, interface, interface))
             commands.append("/ip route add dst-address=%s gateway=ampr-%s" % (dstaddress, interface))
-	    commands.append("/ip neighbor discovery set ampr-%s discover=no" % (interface))
+            commands.append("/ip neighbor discovery set ampr-%s discover=no" % (interface))
 
         if "-v" in sys.argv:
             print "\n".join(commands)
         if "-n" not in sys.argv:
             for command in commands:
                 ssh.exec_command(command)
-		time.sleep(0.1)
+                time.sleep(0.1)
     except socket.timeout:
         print "timeout"
     finally:
